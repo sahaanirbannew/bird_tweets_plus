f9279df13e1829e7939fb638b7d1cc890f11a3a4
Anirban Saha <mailme@anirbansaha.com>
2022-10-12 16:16:55 +0530

    hello world

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask, request, render_template 
+app = Flask(__name__)
+
+@app.route('/home')
+def helloWorld():
+  return "Hello World"
+
+if __name__ == '__main__':
+  app.config['JSONIFY_PRETTYPRINT_REGULAR'] = False
+  app.run(debug=True, port = port) 
