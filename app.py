886240b3a0ec89fa891ad1f2cf65c6bb098ad9b6
Anirban Saha <mailme@anirbansaha.com>
2022-10-13 19:52:43 +0530

    hello world

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
-def add_(a, b):
-  return a+b
+from flask import Flask, request, render_template
+import os
+ON_HEROKU = os.environ.get('ON_HEROKU')
+app = Flask(__name__)
 
 
-print("hello world")
-print(add(4,5))
+@app.route('/ner')
+def getBirds():
+  return "Hello World!"
+  
+  
+  
+if __name__ == '__main__':
+  app.config['JSONIFY_PRETTYPRINT_REGULAR'] = False
+  app.run() 
